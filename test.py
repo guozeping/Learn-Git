3e8c48ac393181611a817a5a0bf2c6ce9352ea04
guozeping <43440405+guozeping@users.noreply.github.com>
2020-03-22 13:51:44 +0800

    second commit

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
 import os
 import time
 import requests
 print(os.cpu_count())
-=======
 aaaaaaaaaa
-
->>>>>>> 9cf13b5... first commit
+bbbbbbbbbb
+cccccccccc
